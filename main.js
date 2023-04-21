0d1d92452e6ba8625f2152d8e70440773aad532d
Madhav Ram <madhav6ram@gmail.com>
2023-04-22 02:43:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,4 +3,4 @@ $chat = "1349956496"
 https://api.telegram.org/bot$token/sendMessage?chat_id=$chat&text=Hello+World
  
 //here is a full example:
-https://api.telegram.org/bot5907380542:AAHHR31xNY2VdezFmbZeSGdTCoUnR64bWJs/sendMessage?chat_id=1349956496&text=Hello+World
+https://api.telegram.org/<API_HERE>/sendMessage?chat_id=<CHAT_ID_HERE>&text=Hello+World
