528290fa9720669ca547ee6e2981dd008b745abc
Madhav Ram <madhav6ram@gmail.com>
2023-04-22 02:45:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-$token = "5907380542:AAHHR31xNY2VdezFmbZeSGdTCoUnR64bWJs"
-$chat = "1349956496"
+$token = ""
+$chat = ""
 https://api.telegram.org/bot$token/sendMessage?chat_id=$chat&text=Hello+World
  
 //here is a full example:
