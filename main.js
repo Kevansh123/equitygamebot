d926ecee53fd20d7bb4831019d85471768749797
Madhav Ram <madhav6ram@gmail.com>
2023-04-22 01:30:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+$token = "5907380542:AAHHR31xNY2VdezFmbZeSGdTCoUnR64bWJs"
+$chat = "1349956496"
+https://api.telegram.org/bot$token/sendMessage?chat_id=$chat&text=Hello+World
+ 
+//here is a full example:
+https://api.telegram.org/bot5907380542:AAHHR31xNY2VdezFmbZeSGdTCoUnR64bWJs/sendMessage?chat_id=1349956496&text=Hello+World
